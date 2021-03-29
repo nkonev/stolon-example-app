6756b73a315367d706b43f0d5d74af9b791f60ca
Nikita Konev <nikit.cpp@yandex.ru>
2021-03-29 11:43:04 +0300

    rm warn

--- src/main/kotlin/name/nkonev/jdbc/stolon/example/app/TestEntity.kt
+++ src/main/kotlin/name/nkonev/jdbc/stolon/example/app/TestEntity.kt
@@ -6,6 +6,6 @@ import org.springframework.data.relational.core.mapping.Table
 @Table("test")
 data class TestEntity(
     @Id
-    var id: Integer? = null,
+    var id: Int? = null,
     var value: String = "",
 )
